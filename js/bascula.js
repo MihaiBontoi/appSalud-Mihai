9292df9ac548abf4af2de5511ff7bbef72642542
Mircea Mihai Bontoi <mirbon2@alu.edu.gva.es>
2022-11-24 19:38:32 +0100

    24/11/22

---
+++ js/bascula.js
@@ -0,0 +1,25 @@
+class Bascula {
+    constructor(peso,altura,fecha ) {
+        this.peso = peso;
+        this.altura = altura;
+        this.fecha = fecha;
+    }
+    obtenerNumeroAnotaciones(){
+
+    }
+    anotarPeso(){
+
+    }
+    obtenerPesoMaximo(){
+
+    }
+    obtenerPesoMinimo(){
+
+    }
+    calcularIMC(){
+
+    }
+    describirIMC(imc){
+        
+    }
+  }
---
+++ js/paciente.js
@@ -0,0 +1,42 @@
+class Paciente{
+    constructor(nombre,apellidos,fechaN){
+        this.nombre=nombre;
+        this.apellidos=apellidos;
+        this.fechaN=fechaN;
+    }
+    saludar(){
+
+    }
+    obtenerNombre(){
+
+    }
+    modificarNombre(nombre){
+
+    }
+    obtenerApellidos(){
+
+    }
+    modificarApellidos(apellidos){
+
+    }
+    obtenerFechaNacimiento(){
+
+    }
+    modificarFechaNacimento(fecha){
+
+    }
+    obtenerEdad(){
+
+    }
+    modificarBascula(bascula){
+
+    }
+    obtenerBascula(){
+
+    }
+    calcularIMC(){
+        
+    }
+
+
+}
